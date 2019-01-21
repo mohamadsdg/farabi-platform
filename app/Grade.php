21ee7d3db688a42a03379745f469560d32f54d01
aliwesome <aliwsome@gmail.com>
2019-01-21 12:50:06 +0330

    Grade completed

---
+++ app/Grade.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    protected $table = 'grades';
+    protected $fillable = [
+        'title',
+        'position',
+        'display'
+    ];
+}
