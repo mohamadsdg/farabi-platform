4ffaf268ae09351d10fb6689dc0c080934b467f2
mohamasdg <sadeghi_mohamadreza@outlook.com>
2019-01-22 17:49:05 +0330

    working on validate

--- static/_plugin/jquery-validation/validate.js
+++ static/_plugin/jquery-validation/validate.js
@@ -12,11 +12,9 @@
             let width = $(element).attr('data-width');
             if (width === 0) {
                 return true;
-            }
-            else if (width >= min_width) {
+            } else if (width >= min_width) {
                 return true;
-            }
-            else if (width < min_width) {
+            } else if (width < min_width) {
                 return false;
             }
         }, "error");
@@ -27,26 +25,25 @@
             let height = $(element).attr('data-height');
             if (height === 0) {
                 return true;
-            }
-            else if (height >= min_height) {
+            } else if (height >= min_height) {
                 return true;
-            }
-            else if (height < min_height) {
+            } else if (height < min_height) {
                 return false;
             }
         }, "error");
 
         // image max_size
         $.validator.addMethod("max_size", function (value, element, max_size) {
-            if (typeof element.files[0] === 'undefined') {return true;}
-            else {
+            if (typeof element.files[0] === 'undefined') {
+                return true;
+            } else {
                 let size = Math.round(element.files[0].size / 1024);
                 return (size <= max_size) ? true : false;
             }
         }, "error");
 
         // checkNationalCode
-        $.validator.addMethod('national_code',function (value) {
+        $.validator.addMethod('national_code', function (value) {
             switch (value) {
                 case '1111111111':
                     return;
@@ -80,10 +77,10 @@
             s = s % 11;
             return (s < 2 && c === s) || (s >= 2 && c === (11 - s));
             // return true;
-        },"error");
+        }, "error");
 
         // checkDebitCard
-        $.validator.addMethod('debit_card',function (value) {
+        $.validator.addMethod('debit_card', function (value) {
             let L = value.length;
             if (L < 16 || parseInt(value.substr(1, 10), 10) === 0 || parseInt(value.substr(10, 6), 10) === 0) return false;
             let c = parseInt(value.substr(15, 1), 10);
@@ -95,8 +92,19 @@
                 s += (d > 9) ? d - 9 : d;
             }
             return ((s % 10) === 0);
-        },"error");
+        }, "error");
 
+        // minimum check checkbox
+        $.validator.addMethod("min_check", function (value, element, min_check) {
+            var $count_check = $(element).closest('[data-wrapper-group]').find('[data-valid-min]:checked').length;
+            return $count_check >= min_check ? true : !(true);
+        }, "error");
+
+        // maximum check checkbox
+        $.validator.addMethod("max_check", function (value, element, max_check) {
+            var $count_check = $(element).closest('[data-wrapper-group]').find('[data-valid-max]:checked').length;
+            return $count_check <= max_check ? true : !(true);
+        }, "error");
         ///////////////// Config Validate
         //valid form
         $("[data-valid-form]").each(function () {
@@ -161,6 +169,7 @@
         });
         /*
          ^[0-9]*$ ===> number
+         09[0|1|2|3|9]([ ]|,|-|[()]){0,2}(?:[0-9]([ ]|,|-|[()]){0,2}){8} ===> phone number iran
          ((0[1-9])|(1[0-2]))[\/-]((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))[\/-](\d{4}) ==> accept birthday
         ^[\u0600-\u06FF\s]+$ ===> persian ascii character
         [\x00-\x80]+ ===> latin ascii character
@@ -219,6 +228,42 @@
                 messages: {
                     national_code: "کد ملی وارد شده صحیح نمیباشد"
                 }
+            });
+        });
+
+        /* validate for count MIN*/
+        $("[data-valid-form] [data-valid-min]").each(function () {
+            let $count = $(this).attr("data-valid-min");
+            $(this).rules('add', {
+                min_check: $count,
+                messages: {
+                    min_check: "حداقل انتخاب " + $count + " مورد الزامی است"
+                }
+            });
+        });
+
+        /* validate for count MAX*/
+        $("[data-valid-form] [data-valid-max]").each(function () {
+            let $count = $(this).attr("data-valid-max");
+            $(this).rules('add', {
+                max_check: $count,
+                messages: {
+                    max_check: "حداکثر انتخاب " + $count + " میباشد"
+                }
+            });
+        });
+
+        /* validate for dependency*/
+        $("[data-valid-form] [data-valid-depend]").each(function () {
+            $(this).on('click', function () {
+                var $valElm = $(this).attr("data-valid-depend");
+                var $selElm = $(this);
+                $($valElm).rules('add', {
+                    required: "#" + $selElm[0].id + ":checked",
+                    messages: {
+                        required: "الزامی است",
+                    }
+                });
             });
         });
 
